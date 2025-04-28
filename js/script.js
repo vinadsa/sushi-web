a592ee67075a44f6f3e2f6c3222564e1bdc29579
vinadsa <kevinadsantoso@gmail.com>
2025-04-28 15:28:36 +0700

    effects update on filters

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ Aos.init({
     duration: 1000,
     offset: 100,
 });
+
+
+const buttons = document.querySelectorAll('.popular-foods__filters-btn')
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        
+        button.classList.add('active');
+    });
+});
+
